24e573d02595280e04d5f6a473390305d9bf2402
Michael <michael.arn@live.fr>
2026-01-29 10:50:04 +0100

    create base project

---
+++ src/main/java/com/marnaud/gaming_coach_booking/GamingCoachBookingApplication.java
@@ -0,0 +1,13 @@
+package com.marnaud.gaming_coach_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingCoachBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingCoachBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marnaud/gaming_coach_booking/GamingCoachBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marnaud.gaming_coach_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamingCoachBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
