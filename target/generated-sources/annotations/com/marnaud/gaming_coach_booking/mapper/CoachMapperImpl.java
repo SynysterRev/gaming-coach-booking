5171b7c9dba28875cdaecd89e8360c8b02b22ba0
Michael <michael.arn@live.fr>
2026-02-02 17:54:13 +0100

    Generate mappers

--- target/generated-sources/annotations/com/marnaud/gaming_coach_booking/mapper/CoachMapperImpl.java
+++ target/generated-sources/annotations/com/marnaud/gaming_coach_booking/mapper/CoachMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-02-02T11:56:30+0100",
+    date = "2026-02-02T17:53:48+0100",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
 )
 @Component
